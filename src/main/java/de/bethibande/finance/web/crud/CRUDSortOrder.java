c67e2b4a8bb81c97e1778fbfc10d2f965c40ee00
bethibande <bethibande@gmail.com>
2025-11-21 14:52:10 +0100

    Create a first test image #22

--- src/main/java/de/bethibande/finance/web/crud/CRUDSortOrder.java
+++ src/main/java/de/bethibande/finance/web/crud/CRUDSortOrder.java
@@ -1,7 +1,9 @@
 package de.bethibande.finance.web.crud;
 
 import io.quarkus.panache.common.Sort;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
+@RegisterForReflection
 public record CRUDSortOrder(
         String field,
         Sort.Direction direction
--- src/main/webui/src/lib/workspace.tsx
+++ src/main/webui/src/lib/workspace.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import {createContext, useContext, useState} from "react";
 import type {Workspace} from "./types.ts";
+import {fetchClient} from "@/lib/api.ts";
+import {showHttpErrorAndContinue} from "@/lib/errors.tsx";
 
 type WorkspaceContextType = {
     workspace: Workspace,
@@ -14,6 +16,22 @@ export const WorkspaceProvider = ({children}: { children: React.ReactNode }) => 
         const stored = window.localStorage.getItem('workspace');
         return stored ? JSON.parse(stored) : null;
     })
+
+    if (!workspace) {
+        // TODO: More graceful handling
+        fetchClient("/api/v1/workspace").then(showHttpErrorAndContinue).then(res => {
+            if (res.ok) {
+                res.json().then(json => {
+                    const data: Workspace[] = json.data
+                    if (data.length > 0) {
+                        setContext(data[0])
+                    } else {
+                        location.href = "/setup"
+                    }
+                });
+            }
+        })
+    }
 
     const setContext = (newContext: Workspace) => {
         setWorkspace(newContext)
