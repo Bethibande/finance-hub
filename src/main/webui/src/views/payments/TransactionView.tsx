ca8e9f81e0937a0cc4a75ad772ef24f92f1b9a8a
bethibande <bethibande@gmail.com>
2025-12-05 15:51:57 +0100

    Quick book action for payments #28

--- src/main/webui/src/views/payments/TransactionView.tsx
+++ src/main/webui/src/views/payments/TransactionView.tsx
@@ -1,16 +1,32 @@
 import type {ColumnDef} from "@tanstack/react-table";
-import type {TransactionDTOExpanded} from "@/generated";
+import {
+    type BookedAmountDTO,
+    type TransactionDTOExpanded,
+    type TransactionDTOWithoutBookedAmounts,
+    TransactionStatus
+} from "@/generated";
 import i18next from "i18next";
 import {EntityList} from "@/components/entity/entity-list.tsx";
 import {TransactionAPI, TransactionFunctions} from "@/views/payments/TransactionFunctions.ts";
 import {TransactionFormExpanded} from "@/views/payments/TransactionForm.tsx";
 import {renderAmount, renderDate} from "@/components/data-table.tsx";
-import {useState} from "react";
+import {type FunctionComponent, useState} from "react";
 import {BookedAmountsDialog} from "@/views/payments/BookedAmountsDialog.tsx";
 import {DropdownMenuItem} from "@/components/ui/dropdown-menu.tsx";
 import {showError} from "@/lib/errors.tsx";
 import {StatusToIcon} from "@/views/payments/TransactionItem.tsx";
 import {PartnerFunctions} from "@/views/partner/PartnerFunctions.ts";
+import {EntityDialog, EntityDialogState, type EntityFormProps} from "@/components/entity/entity-dialog.tsx";
+import {BookedAmountForm} from "@/views/payments/BookedAmountForm.tsx";
+
+export function deflate(dto: TransactionDTOExpanded): TransactionDTOWithoutBookedAmounts {
+    return {
+        ...dto,
+        assetId: dto.asset?.id!,
+        walletId: dto.wallet?.id!,
+        partnerId: dto.partner?.id,
+    }
+}
 
 export function TransactionView() {
     const columns: ColumnDef<TransactionDTOExpanded>[] = [
@@ -24,7 +40,8 @@ export function TransactionView() {
             id: "status",
             accessorKey: "status",
             header: i18next.t("transaction.status"),
-            cell: ({row}) => (<span className={"flex gap-1.5 items-center"}>{StatusToIcon[row.original.status]} {i18next.t("TransactionStatus." + row.original.status)}</span>),
+            cell: ({row}) => (<span
+                className={"flex gap-1.5 items-center"}>{StatusToIcon[row.original.status]} {i18next.t("TransactionStatus." + row.original.status)}</span>),
             enableSorting: true,
         },
         {
@@ -80,8 +97,31 @@ export function TransactionView() {
         }
     }
 
+
+    const [entityDialogStatus, setEntityDialogStatus] = useState<EntityDialogState>(EntityDialogState.Closed);
+    const bookedAmountForm: FunctionComponent<EntityFormProps<BookedAmountDTO>> = (props) => (
+        <BookedAmountForm transaction={bookedAmountStatus!} {...props}/>
+    )
+
+    function complete() {
+        if (bookedAmountStatus) {
+            TransactionAPI.apiV2TransactionPatch({
+                transactionDTOWithoutWorkspaceAndBookedAmounts: {
+                    ...deflate(bookedAmountStatus),
+                    status: TransactionStatus.Closed,
+                }
+            }).then(() => setVersion(version + 1)).catch(showError);
+        }
+    }
+
     return (
         <>
+            <EntityDialog Form={bookedAmountForm}
+                          state={entityDialogStatus}
+                          setState={setEntityDialogStatus}
+                          entity={null}
+                          i18nKey={"booked"}
+                          onSubmit={complete}/>
             {bookedAmountStatus && (
                 <BookedAmountsDialog transaction={bookedAmountStatus}
                                      open={baDialogOpen}
@@ -99,6 +139,14 @@ export function TransactionView() {
                                 }}>
                                     {i18next.t("amounts.edit.title")}
                                 </DropdownMenuItem>
+                                {row.original.status === TransactionStatus.Open && (
+                                    <DropdownMenuItem onClick={() => {
+                                        setBookedAmountStatus(row.original)
+                                        setEntityDialogStatus(EntityDialogState.Creating)
+                                    }}>
+                                        {i18next.t("transaction.complete")}
+                                    </DropdownMenuItem>
+                                )}
                             </>
                         )}
                         i18nKey={"transaction"}
